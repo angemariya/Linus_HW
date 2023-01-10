f59fa7209b5bb8a7d6853787e22ffd485a70048f
Maria Angelova <angelova.m@hotmail.com>
2023-01-10 20:54:37 +0100

    Add files via upload

---
+++ HW_LINUX_23122022/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /tmp/MARIA_
+touch /tmp/MARIA_/new2023year.txt
+date >> /tmp/MARIA_/new2023year.txt
+echo "it works" >> /tmp/MARIA_/new2023year.txt
+cp /tmp/MARIA_/new2023year.txt /opt/new2023year.txt
